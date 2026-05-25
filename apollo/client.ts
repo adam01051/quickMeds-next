a52d7aaaa3ef998708b1a1b716a005f4965070fb
adam01051 <lloki0105@gmail.com>
2026-05-25 23:53:11 +0900

    feat: develop websocket chatting client fisrt stage

--- apollo/client.ts
+++ apollo/client.ts
@@ -28,6 +28,38 @@ const tokenRefreshLink = new TokenRefreshLink({
 	},
 });
 
+
+
+// Custom WebSocket client
+class LoggingWebSocket {
+  private socket: WebSocket;
+
+  constructor(url: string) {
+    this.socket = new WebSocket(url);
+
+    this.socket.onopen = () => {
+      console.log('WebSocket connection!');
+    };
+
+    this.socket.onmessage = (msg) => {
+      console.log('WebSocket message:', msg.data);
+    };
+
+    this.socket.onerror = (error) => {
+      console.log('WebSocket, error:', error);
+    };
+  }
+
+  send(data: string | ArrayBuffer | SharedArrayBuffer | Blob | ArrayBufferView) {
+    this.socket.send(data as any);
+  }
+
+  close() {
+    this.socket.close();
+  }
+}
+
+
 function createIsomorphicLink() {
 	if (typeof window !== 'undefined') {
 		const authLink = new ApolloLink((operation, forward) => {
